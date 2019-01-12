a594f7c384d72310632e42dd9d3b4976b99e671d
Mucheng910 <3544306302@qq.com>
2019-01-12 10:34:47 +0800

    +webpack

---
+++ dist/precache-manifest.75c3d188c51883797108b6404f8f9e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "21724a4eeb90dff2b953",
+    "url": "./static/js/main.21724a4e.chunk.js"
+  },
+  {
+    "revision": "ea7f6c60be25a10b0a14",
+    "url": "./static/js/1.ea7f6c60.chunk.js"
+  },
+  {
+    "revision": "ea7f6c60be25a10b0a14",
+    "url": "./static/css/1.b15cece5.chunk.css"
+  },
+  {
+    "revision": "d5068c91a64e452530a8d2c152788905",
+    "url": "./index.html"
+  }
+];
